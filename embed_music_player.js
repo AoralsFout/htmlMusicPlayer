d96ed7a756fcac40968d9a9b010dd91a176d1f80
AoralsFout <102973374+AoralsFout@users.noreply.github.com>
2025-02-06 14:49:30 +0800

    Update embed_music_player.js

--- embed_music_player.js
+++ embed_music_player.js
@@ -547,7 +547,7 @@ function embedMusicPlayer() {
 
     async function getMusicInfo() {
       try {
-        const response = await fetch("https://cdn.jsdelivr.net/gh/Aoralsfout/htmlMusicPlayer/getMusicInf.php",
+        const response = await fetch("",
         {
           headers: {
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
